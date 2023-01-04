25138ac5159d95951a22d8ba05ab3cbfc0830b59
r.danna <rafdanna28@gmail.com>
2023-01-04 11:42:58 +0100

    style su edit della spesa con aggiunta del bottone delete

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
-import {Text} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {useLayoutEffect} from "react";
+import IconButton from "../components/UI/IconButton";
+import {GlobalStyles} from "../constants/styles";
 
 function ManageExpense({route, navigation}){
     const editedExpenseId = route.params?.expenseId;
@@ -10,9 +12,40 @@ function ManageExpense({route, navigation}){
         })
     }, [navigation, isEditing]);
 
+    function deleteExpenseHandler(){
+
+    }
+
     return (
-        <Text>ManageExpense Screen</Text>
+        <View style={styles.container}>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPressButton={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+        </View>
     );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary500
+    },
+
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: "center"
+    }
+});
